b9c66039c37c6f7f9cfa5082395374032c7b73ca
Allon Hadaya <allonhadaya@hotmail.com>
2012-04-25 14:44:02 -0400

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollegeWallet::Application.config.secret_token = 'f96a1fd955cd127c61d8a84a3abf29c2d2b87b415aa43a4fe96a55ee09ad113657a5ce382ce2a46a94ce8631e3455e7c93f60758e9d7c711886298493e38e546'
